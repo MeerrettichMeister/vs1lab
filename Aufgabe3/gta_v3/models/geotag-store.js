8f47eaf4c23425f104f8b0e9984a3375f7f083f6
Christophe <christophe.misc+git@protonmail.ch>
2024-06-08 18:59:26 +0200

    fix: 3.2.1a distance calculation

--- Aufgabe3/gta_v3/models/geotag-store.js
+++ Aufgabe3/gta_v3/models/geotag-store.js
@@ -46,14 +46,44 @@ class InMemoryGeoTagStore {
         this.#tagList.splice(this.#tagList.indexOf(this.#tagList.reduce((el) => el.name === name)), 1);
     }
 
-    getNearbyGeoTags(lat, long, radius){
-        return this.#tagList.filter((el) => {
-            return (Math.sqrt(Math.pow((lat - el.lat),2) + Math.pow((long - el.long),2)) <= radius);
-        })
+    /**
+     * @param {number} degrees
+     */
+    #degreesToRadian(degrees) {
+        return degrees * (Math.PI / 180);
     }
 
-    searchNearbyGeoTags(lat, long, radius, seatchTerm){
-        return this.getNearbyGeoTags(lat,long,radius).filter((el) => {
+    /**
+     * Calculate distance from two points using Haversine formula
+     *
+     * Based on https://stackoverflow.com/a/27943
+     *
+     * @param {number} pointA.lat
+     * @param {number} pointA.lon
+     * @param {number} pointB.lat
+     * @param {number} pointB.lon
+     */
+    #distance(pointA, pointB) {
+        // Radius of the earth in km
+        const earthRadius = 6371;
+        const dLat = this.#degreesToRadian(pointB.lat - pointA.lat);
+        const dLon = this.#degreesToRadian(pointB.lon - pointA.lon);
+        const a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(this.#degreesToRadian(pointA.lat)) * Math.cos(this.#degreesToRadian(pointB.lat)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        ;
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        // Distance in km
+        return earthRadius * c;
+    }
+
+    getNearbyGeoTags(lat, long, radius) {
+        return this.#tagList.filter((el) => (this.#distance({lat, lon: long}, {lat: el.lat, lon: el.long}) <= radius))
+    }
+
+    searchNearbyGeoTags(lat, long, radius, seatchTerm) {
+        return this.getNearbyGeoTags(lat, long, radius).filter((el) => {
             return (el.name.contains(seatchTerm) || el.tag.contains(seatchTerm))
         })
     }
